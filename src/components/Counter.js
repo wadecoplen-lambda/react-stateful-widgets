0315a4c0033d3e52e345ff062e81ceae358a41dd
Wade-Coplen <wadecoplen@gmail.com>
2020-08-21 23:22:45 -0700

    I DID IT

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -50,28 +50,35 @@ import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Counter() {
   /* STEP 1 */
-  const [count, setCount] = useSate(0)
+  const [count, setCount] = useState(0)
 
   const increment = () => {
     /* STEP 4 */
+    setCount(count + 1)
   };
   const decrement = () => {
     /* STEP 5 */
+    setCount(count - 1)
   };
   const reset = () => {
     /* STEP 6 */
-  };
+    setCount(0)
 
+  };
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: {count} % 2 === 0 ? 'royalblue' : 'crimson'
+    color: count % 2 === 0 ? 'royalblue' : 'crimson'
   };
+  const isEven = count % 2 === 0 ? 'Even' : null
+  const isOdd = count % 2 === 1 ? 'Odd' : null
 
   return (
     <div className='widget-counter container'>
       <h2>Counter</h2>
-      <div style={style}>Number 0 is even</div> {/* STEP 3 */}
+      <div style={style}>
+        Number {count} is {isEven} {isOdd} 
+      </div> {/* STEP 3 */}
       <div>
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
